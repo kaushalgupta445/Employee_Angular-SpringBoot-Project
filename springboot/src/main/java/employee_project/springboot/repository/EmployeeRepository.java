b599103e083337e9c4ffd6ba9d6a4102bbf75cf4
kaushalgupta445 <105626593+kaushalgupta445@users.noreply.github.com>
2024-07-12 19:01:15 +0530

    Adding Intial Phase of Angular + SpringBoot CRUD App

---
+++ springboot/src/main/java/employee_project/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package employee_project.springboot.repository;
+
+import employee_project.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
